107f4a6e462a0fea7510c10eec26613389fa3a35
Iced <leandrobruzzi@gmail.com>
2023-01-19 19:59:14 -0300

    agrego header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
+import HeaderComponent from "../components/HeaderComponent";
 
 const HomePage = () => {
     return(
         <section className="container-fluid">
+            <HeaderComponent />
             <h1>Home Page</h1>
         </section>
     );   
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
+import HeaderComponent from "../components/HeaderComponent";
 
 const ProductsPage = () => {
     return(
         <section className="container-fluid">
+            <HeaderComponent />
             <h1>Product Page</h1>
         </section>
     );   
